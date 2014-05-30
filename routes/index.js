bb09958a9a663aad75e0e090cba1aca987d84167
Charles Lennon <charleslcsantos@gmail.com>
2014-05-30 10:05:22 -0300

    app ok

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,37 @@ var router = express.Router();
 
 var soap = require('soap');
 //var url = "http://wsf.cdyne.com/WeatherWS/Weather.asmx?wsdl";
-var url = __dirname+"/Calculadora.wsdl";
+var url = "http://192.168.0.101:8080/Calculadora/services/Calculadora?wsdl";
 var args;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'WebService' });
+  res.render('index', { title: 'WebService', result: '0' });
 });
 
 /* Post Calcular. */
 router.post('/', function(req, res) {
 	args = {n1: req.body.valor1, n2: req.body.valor2};
-	soap.createClient(url, function(err, client) {
-		console.log("req    - " + req.body.op);
+	soap.createClient(url, function(err, client) { 
 		if(req.body.op == "soma"){
 			client.soma(args, function(err, result) {
 			      // res.send(result.somaReturn); 
 			      res.render('index', { title: 'WebService', result: result.somaReturn || '0' });
 			});
-		} else {
-			console.log("entrei no else");
-			client.sub(args, function(err, result) {
-				console.log("entrei na function ");
+		} else if(req.body.op == "subtracao") {
+			client.sub(args, function(err, result) { 
 			      // res.send(result.somaReturn); 
-			      res.render('index', { title: 'WebService', result: result.subReturn || '0' });
-			      console.log(result);
+			      res.render('index', { title: 'WebService', result: result.subReturn || '0' }); 
+			});
+		} else if(req.body.op == "multiplicar") {
+			client.multiplicar(args, function(err, result) {
+			      // res.send(result.somaReturn); 
+			      res.render('index', { title: 'WebService', result: result.multiplicarReturn || '0' }); 
+			});
+		} else if(req.body.op == "dividir") {
+			client.dividir(args, function(err, result) {
+			      // res.send(result.somaReturn); 
+			      res.render('index', { title: 'WebService', result: result.dividirReturn || '0' });
 			});
 		}
 	}); 
